06ffef95cc31586e86c5732aaaf8c4d57f54e120
ravi <asipiravi3@gmail.com>
2022-01-11 23:40:59 +0530

    project completion

--- module5/banking.py
+++ module5/banking.py
@@ -1,15 +1,20 @@
 #Banking Application
-import bankingFunctions
+import bankingFunctions as BF
 
 print("###################\nWelcome to ABSA \n###################\n")
 menuList = ["Check Balance","Add Balance","Withdraw Balance","Exit"]
 balance = 0
 
-bankingFunctions.menu(menuList)
-userInput = bankingFunctions.getInput(menuList)
-
-if userInput== 1:
-    bankingFunctions.checkBalance(balance)
-else:
-    print('options not working')
+while True:
+    BF.menu(menuList)
+    userInput = BF.getInput(menuList)
+    
+    if userInput == 1:
+        BF.checkBalance(balance)
+    elif userInput == 2:
+        balance = BF.addBalance(balance)
+    elif userInput == 3:
+        balance = BF.withdrawBalance(balance)
+    else:
+        break
 
--- module5/bankingFunctions.py
+++ module5/bankingFunctions.py
@@ -14,3 +14,26 @@ def getInput(menuList):
     
 def checkBalance(balance):
     print("Current Balance : {}".format(balance))
+    
+def addBalance(balance):
+    addAmount = int(input("enter amount to add : "))
+    if (addAmount>0):
+        balance += addAmount
+    else:
+        print("Add amount should not be negative")
+        
+    print("Current Balance: {}".format(balance))
+    return balance
+    
+def withdrawBalance(balance):
+    withdrawAmount = int(input("enter amount to withdraw : "))
+    if withdrawAmount>0 :
+        if withdrawAmount<=balance:
+            balance -= withdrawAmount
+        else:
+            print("withdraw amount should not be greater than avaialable balance")
+    else:
+        print("withdraw amount should not be negative")
+        
+    print("Current Balance: {}".format(balance))
+    return balance
